e05d741241f8c4016e36b8305a70ef5259679564
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-01-22 13:57:13 +0100

    checked some stuff with the vue.js website

--- src/vue/VueRouter.hx
+++ src/vue/VueRouter.hx
@@ -6,11 +6,21 @@ extern class VueRouter {
 }
 typedef Route = {
 	var path:String;
-	var component:VueComponent;
+	@:optional var component:VueComponent;
 	@:optional var name:String;
+
+
+// components?: { [name: string]: Component }; // for named views
+// redirect?: string | Location | Function;
+// alias?: string | Array<string>;
+// children?: Array<RouteConfig>; // for nested routes
+// beforeEnter?: (to: Route, from: Route, next: Function) => void;
+// meta?: any;
+
+
 }
 typedef RouterOption = {
-	var mode:String;
+	var mode:String; // 'history','hash','abstract'
 	var base:Dynamic;
 	var routes:Array<Route>;
 }
