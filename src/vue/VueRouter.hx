fe390992afcaf3615abf06d6707412c191569f2c
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-01-25 10:39:48 +0100

    this might be a mistake… need to focus on 2.0

--- src/vue/VueRouter.hx
+++ src/vue/VueRouter.hx
@@ -1,26 +1,45 @@
 package vue;
 
+import haxe.extern.EitherType;
+import js.html.HtmlElement;
+
+/**
+ * vue-router@2.x only works with Vue 2.x.
+ */
 @:native("VueRouter")
 extern class VueRouter {
 	public function new (routeOptions:Dynamic);
 }
+
+// https://router.vuejs.org/en/api/route-object.html
 typedef Route = {
 	var path:String;
 	@:optional var component:VueComponent;
 	@:optional var name:String;
-
-
-// components?: { [name: string]: Component }; // for named views
-// redirect?: string | Location | Function;
-// alias?: string | Array<string>;
-// children?: Array<RouteConfig>; // for nested routes
-// beforeEnter?: (to: Route, from: Route, next: Function) => void;
-// meta?: any;
-
-
+	@:optional var params:Dynamic;
+	@:optional var query:Dynamic;
+	@:optional var hash:String;
+	@:optional var fullPath:String;
+	@:optional var matched:Array<RouteRecord>;
 }
 typedef RouterOption = {
 	var mode:String; // 'history','hash','abstract'
 	var base:Dynamic;
 	var routes:Array<Route>;
+}
+typedef RouteRecord = {
+	// no idea what it does
+}
+
+
+@:native('this')
+extern class RouterCtx {
+
+	@:native("$route") public var route:Route;
+
+	// static var userId(default, null):String;
+	// var connection(default, null):Dynamic;
+	// static function setUserId(?userId:String):Void;
+	// static var isSimulation(default, null):Bool;
+	// static var unBlock(default, null):Bool;
 }
